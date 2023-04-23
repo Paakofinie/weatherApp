9c6dc346cc5af3ffcceab0e776029df49b37034e
Felix Otu Kofi Agyenkwah <94941486+Paakofinie@users.noreply.github.com>
2023-04-23 13:01:06 +0000

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const apiKey = 'c1de6e0aa4a172d8f4c747c793744958';
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState('');
+
+  const getWeather = (event) => {
+    if (event.key === 'Enter') {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          setWeatherData(data);
+          setCity('');
+        })
+        .catch(error => console.error(error));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input 
+        className="input" 
+        placeholder="Enter City..."
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      />
+
+      {Object.keys(weatherData).length === 0 ? (
+        <div>
+          <p>Welcome to weather app! Enter in a city to get the weather of</p>
+        </div>
+      ): (
+        <div className='weather-data'>
+          <p className='city'>{weatherData.name}</p>
+          <p className='temp'> {Math.round(weatherData.main?.temp)}°C</p>
+          <p className='weather'>{weatherData.weather?.[0]?.main}</p>
+        </div> 
+      )}
+
+      {weatherData.cod === '404' && (
+        <p> City not found.</p>
+      )}
+
     </div>
   );
 }
